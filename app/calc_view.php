c0a027693273858c6239f5cdfa320e0314d29b55
mziarkiewicz <74470450+mziarkiewicz@users.noreply.github.com>
2021-02-11 00:28:44 +0100

    Projekt kalkulatora kredytowego - wersja 2.0 - zmiana wyglądu i praca nad ochroną dostępu - final commit

--- app/calc_view.php
+++ app/calc_view.php
@@ -1,4 +1,6 @@
-<?php require_once dirname(__FILE__) .'/../config.php';?>
+<?php
+
+?>
 <!DOCTYPE HTML>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
 <head>
@@ -19,15 +21,15 @@
         <fieldset>
             <div class="pure-control-group">
                 <label for="id_val">Kwota kredytu: </label>
-                <input id="id_val" type="text" name="amo" value="<?php print(isset($amo) ? $amo : '' ); ?>" /><br />
+                <input id="id_val" type="text" name="amo" value="<?php out($amo); ?>" /><br />
             </div>
             <div class="pure-control-group">
                 <label for="id_yr">Liczba lat: </label>
-                <input id="id_yr" type="text" name="yr" value="<?php print(isset($yr) ? $yr : '' ); ?>" /><br />
+                <input id="id_yr" type="text" name="yr" value="<?php out($yr ); ?>" /><br />
             </div>
             <div class="pure-control-group">
                 <label for="id_pct">Oprocentowanie: </label>
-                <input id="id_pct" type="text" name="pct" value="<?php print(isset($pct) ? $pct : '' ); ?>" /><br />
+                <input id="id_pct" type="text" name="pct" value="<?php out($pct ); ?>" /><br />
             </div>
             <div class="pure-controls">
                 <input class="pure-button pure-button-primary" type="submit" value="Oblicz" />
@@ -38,14 +40,14 @@
     <?php
     //wyświeltenie listy błędów, jeśli istnieją
     if (isset($messages)) {
-//	if (count ( $messages ) > 0) {
+	if (count ( $messages ) > 0) {
         echo '<ol style="background: #ff8888; margin: 1em; margin-top: 2em; margin-right: 0px; margin-bottom: 2em; margin-left: 0px; padding: .3em 1em; border-radius: 5px; color: #fff;">';
         foreach ( $messages as $key => $msg ) {
             echo '<li>'.$msg.'</li>';
         }
         echo '</ol>';
     }
-    //}
+    }
     ?>
 
     <?php if (isset($result)){ ?>
@@ -55,8 +57,5 @@
     <?php } ?>
 
 </div>
-
-
-
 </body>
 </html>
